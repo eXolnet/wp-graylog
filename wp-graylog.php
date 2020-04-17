f1cb20df11145076e23dd12262ff470153c824b3
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2020-04-17 16:57:57 -0400

    Add mu-plugin metadata in wp-graylog.php

--- wp-graylog.php
+++ wp-graylog.php
@@ -1,4 +1,12 @@
 <?php
+/*
+Plugin Name:  wp-graylog
+Plugin URI:   https://github.com/eXolnet/wp-graylog
+Description:  Adds a mu-plugin that register a Monolog handler to send exception to a Graylog channel.
+Version:      1.0.0
+Author:       eXolnet
+Author URI:   https://www.exolnet.com
+*/
 
 use Exolnet\Wordpress\Graylog\WpGraylog;
 
